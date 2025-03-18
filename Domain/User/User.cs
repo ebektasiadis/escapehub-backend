5b137ee6314341f7ebc3a3f7aa179a2f6c8686ad
Efstratios Bektasiadis <ebektasiadis@gmail.com>
2025-03-18 20:29:45 +0100

    feat(users): added email and password field on table

--- Domain/User/User.cs
+++ Domain/User/User.cs
@@ -6,6 +6,24 @@ public sealed class User
     public string IdentityProviderId { get; private set; }
     public string FirstName { get; private set; }
     public string LastName { get; private set; }
+    public string Email { get; private set; }
+    public string Password { get; private set; }
 
     private User() { }
+
+    private User(UserId id, string identityProviderId, string firstName, string lastName, string email, string password)
+    {
+        Id = id;
+        IdentityProviderId = identityProviderId;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Password = password;
+    }
+
+    public static User Create(string identityProviderId, string firstName, string lastName, string email, string password)
+    {
+        UserId userId = new UserId(new Ulid());
+        return new User(userId, identityProviderId, firstName, lastName, email, password);
+    }
 }
---
+++ Infrastructure/Database/Migrations/20250318192751_Added_Email_Password_To_User_Table.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_Email_Password_To_User_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "email",
+                schema: "public",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "password",
+                schema: "public",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "email",
+                schema: "public",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "password",
+                schema: "public",
+                table: "users");
+        }
+    }
+}
