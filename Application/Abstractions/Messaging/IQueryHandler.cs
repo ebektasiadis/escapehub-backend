86ba93f70f7e9e15f0f9c1afcdc1f454ef042735
Efstratios Bektasiadis <ebektasiadis@gmail.com>
2025-03-18 19:40:00 +0100

    misc(configs): updated .editorconfig and formatted codebase

--- Application/Abstractions/Messaging/IQueryHandler.cs
+++ Application/Abstractions/Messaging/IQueryHandler.cs
@@ -3,4 +3,4 @@ using SharedKernel;
 
 namespace Application.Abstractions.Messaging;
 
-public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, Result<TResponse>> where TQuery: IQuery<TResponse>;
+public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, Result<TResponse>> where TQuery : IQuery<TResponse>;
